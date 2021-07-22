10b6c6635a7bb58574526f2cfd8a4bb865dc3d55
MastCar <csivanj20077@gmail.com>
2021-07-22 16:38:50 -0500

    idea

--- back/resources/views/categorias.blade.php
+++ back/resources/views/categorias.blade.php
@@ -16,7 +16,7 @@
                         <div class="kt-portlet__head kt-portlet__head--lg">
                             <div class="kt-portlet__head-label">
                                 <span class="kt-portlet__head-icon"><i class="fa fa-check"></i></span>
-                                <h3 class="kt-portlet__head-title">Listado de Registros</h3>
+                                <h3 class="kt-portlet__head-title">Listado de Registross</h3>
                             </div>
                             <div class="kt-portlet__head-toolbar">
                                 <div class="kt-portlet__head-wrapper">
